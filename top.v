924f2ee5f98367d90ea55645a91e7ac6a22e3235
nickbild <Nicholas.Bild@sprint.com>
2020-06-06 18:28:57 -0400

    Save progress.

--- top.v
+++ top.v
@@ -5,6 +5,7 @@ module top (
     // USB pull-up resistor
     output USBPU,
 
+    // GPIO Outputs.
     output PIN_8,
     output PIN_9,
     output PIN_10,
@@ -12,24 +13,38 @@ module top (
     output PIN_12,
     output PIN_13,
 
+    // GPIO Inputs.
+    input PIN_1,
+    input PIN_2,
+    input PIN_3,
+    input PIN_4,
+    input PIN_5,
+    input PIN_6,
+    input PIN_7,
+    input PIN_14,
+    input PIN_15,
+    input PIN_16,
+    input PIN_17,
+    input PIN_18,
+    input PIN_19,
+    input PIN_20,
+    input PIN_21,
+    input PIN_22,
+    input PIN_23,
+    input PIN_24,
     input PIN_31
-
 );
 
     // drive USB pull-up resistor to '0' to disable USB
     assign USBPU = 0;
 
-    // Outputs.
+    // VGA output signals.
     wire clk_10mhz;
     assign PIN_8 = red;
     assign PIN_9 = green;
     assign PIN_10 = blue;
     assign PIN_12 = h_sync;
     assign PIN_13 = v_sync;
-
-    // Inputs.
-    wire interrupt;
-    assign PIN_31 = interrupt;
 
     reg [8:0] h_counter;
     reg [0:0] h_sync;
@@ -40,6 +55,57 @@ module top (
     reg [0:0] red;
     reg [0:0] green;
     reg [0:0] blue;
+
+    // Interrupt input.
+    wire interrupt;
+    assign interrupt = PIN_31;
+
+    // Address inputs.
+    wire a0;
+    wire a1;
+    wire a2;
+    wire a3;
+    wire a4;
+    wire a5;
+    wire a6;
+    wire a7;
+    wire a8;
+    wire a9;
+    wire a10;
+    wire a11;
+    wire a12;
+    wire a13;
+    wire a14;
+
+    assign a0 = PIN_1;
+    assign a1 = PIN_2;
+    assign a2 = PIN_3;
+    assign a3 = PIN_4;
+    assign a4 = PIN_5;
+    assign a5 = PIN_6;
+    assign a6 = PIN_7;
+    assign a7 = PIN_14;
+    assign a8 = PIN_15;
+    assign a9 = PIN_16;
+    assign a10 = PIN_17;
+    assign a11 = PIN_18;
+    assign a12 = PIN_19;
+    assign a13 = PIN_20;
+    assign a14 = PIN_21;
+
+    reg [2:0] memory_array [0:30000];
+    reg [14:0] address;
+
+    // RGB data inputs.
+    wire r_in;
+    wire g_in;
+    wire b_in;
+
+    assign r_in = PIN_22;
+    assign g_in = PIN_23;
+    assign b_in = PIN_24;
+
+    reg [2:0] rgb_in;
 
     // Create a 10MHz clock.
     // http://martin.hinner.info/vga/timing.html
@@ -65,13 +131,9 @@ module top (
       .BYPASS(0)
     );
 
-    reg [2:0] memory_array [0:30000];
-    reg [7:0] address;
-
     // Zero out display.
     initial begin
       $readmemb("memory_array.mem", memory_array);
-      address <= 0;
     end
 
     // <= : every line executed in parallel in always block
@@ -128,9 +190,15 @@ module top (
     end
 
     // Load pixel data into memory.
-    always @(negedge interrupt) begin
-      memory_array[address] <= 3'b111;
-      address <= address + 1'b1;
+    always @(posedge interrupt) begin
+      // Convoluted method of concatenating GPIO pin bits to get an address -- keeps place and route
+      // working without using too many LUs for the TinyFPGA.
+      address = (a14 * 2**14) + (a13 * 2**13) + (a12 * 2**12) + (a11 * 2**11) + (a10 * 2**10) + (a9 * 2**9) + (a8 * 2**8);
+      address = address + (a7 * 2**7) + (a6 * 2**6) + (a5 * 2**5) + (a4 * 2**4) + (a3 * 2**3) + (a2 * 2**2) + (a1 * 2**1) + (a0 * 2**0);
+
+      rgb_in = (b_in * 2**2) + (g_in * 2**1) + (r_in * 2**0);
+
+      memory_array[address] <= rgb_in;
     end
 
 endmodule
